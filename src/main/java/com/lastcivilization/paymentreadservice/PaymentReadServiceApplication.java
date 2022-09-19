b90463e84d62ae1c74e0dd3f09563fa965499d63
mrrevo55 <kwolny31@gmail.com>
2022-09-19 21:43:36 +0200

    add connection configuration

--- src/main/java/com/lastcivilization/paymentreadservice/PaymentReadServiceApplication.java
+++ src/main/java/com/lastcivilization/paymentreadservice/PaymentReadServiceApplication.java
@@ -2,8 +2,10 @@ package com.lastcivilization.paymentreadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentReadServiceApplication {
 
 	public static void main(String[] args) {
