3a769f567c39a094d05d89c276151e60c8607904
mrrevo55 <kwolny31@gmail.com>
2022-09-19 21:39:46 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/paymentreadservice/PaymentReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.paymentreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcivilization/paymentreadservice/PaymentReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.paymentreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
